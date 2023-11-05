e4030673733a90d181ca74d6052d901482088032
Dark-Legends <142109781+Dark-Legends@users.noreply.github.com>
2023-11-05 13:37:26 +0530

    Update install.sh
    All error fix

--- install.sh
+++ install.sh
@@ -34,7 +34,6 @@ for ((i=1; i<=5; i++))
 do
 echo
 done
-
 }
 
 # Detect distribution
@@ -59,7 +58,6 @@ do
 echo
 done
 	echo -e "\033[1;32m"
-
 echo -e "\t⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿"
 echo -e "\t⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠈⠻⢿⠿⠋⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿"
 echo -e "\t⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿"
@@ -83,7 +81,7 @@ do
 echo
 done
 
-	echo -e "\t ${R}WELCOME TO LINUX PACKAGE MANGER "
+	echo -e "\t ${R}WELCOME TO LINUX PACKAGE MANGER"
 for ((i=1; i<=5; i++))
 do
 echo
@@ -115,184 +113,99 @@ echo -e "\t${Y}8.  ${G}Figlet\n"
 echo -e "\t${Y}9.  ${G}Git\n"
 echo -e "\t${Y}10. ${G}Nano\n"
 echo -e "\t${Y}11. ${G}Lolcat\n" 
-echo -e "\t${Y}12. ${G}SSh "
-echo
-echo -e "\t${Y}13. ${G}Wget "
-echo
-echo -e "\t${Y}14. ${G}curl "
-echo
-echo -e "\t${Y}15. ${G}Nmap "
-echo
-echo -e "\t${Y}16. ${G}tmux "
-echo
-echo -e "\t${Y}17. ${G}fish "
-echo
-echo -e "\t${Y}18. ${G}htop "
-echo
-echo -e "\t${Y}19. ${G}apache "
-echo
-echo -e "\t${Y}20. ${G}toilet "
-echo
-echo -e "\t${Y}21. ${G}vim "
-echo
-echo -e "\t${Y}22. ${G}Netcat "
-echo
-echo -e "\t${Y}23. ${G}sl "
-echo
-echo -e "\t${Y}24. ${G}Zip "
-echo
-echo -e "\t${Y}25. ${G}UnZip "
-echo
-echo -e "\t${Y}26. ${G}Java "
-echo
-echo -e "\t${Y}27. ${G}Exiftool "
-echo
-echo -e "\t${Y}28. ${G}Libcaca "
-echo
-echo -e "\t${Y}29. ${G}Termimage "
-echo
-echo -e "\t${Y}30. ${G}w3m "
-echo
-echo -e "\t${Y}31. ${G}fortune "
-echo
-echo -e "\t${Y}32. ${G}Cmatrix "
-echo
-echo -e "\t${Y}33. ${G}Screenfetch"
-echo
-echo -e "\t${Y}34. ${G}Tor "
-echo
-echo -e "\t${Y}35. ${G}tty-clock "
-echo
-echo -e "\t${Y}36. ${G}neofetch "
-echo
-echo -e "\t${Y}37. ${G}Moon-buggy "
-echo
-echo -e "\t${Y}38. ${G}Clang "
-echo
-echo -e "\t${Y}39. ${G}Proot-Distro "
-echo
-echo -e "\t${Y}40. ${G}Termux-api"
-echo
-echo -e "\t${Y}41. ${G}Cowsay "
-echo
-echo -e "\t${Y}42. ${G}Oneko "
-echo
-echo -e "\t${Y}43. ${G}Xeyes"
-echo
-echo -e "\t${Y}44. ${G}Hollywood "
-echo
-echo -e "\t${Y}45. ${G}Httrack "
-echo
-echo -e "\t${Y}46. ${G}mlocate "
-echo
-echo -e "\t${Y}47. ${G}Neovim "
-echo
-echo -e "\t${Y}48. ${G}Hashcat "
-echo
-echo -e "\t${Y}49. ${G}Hydra "
-echo
-echo -e "\t${Y}50. ${G}Metasploit-Framework "
-echo
-echo -e "\t${Y}51. ${G}Sqlmap "
-echo
-echo -e "\t${Y}52. ${G}John the Ripper "
-echo
-echo -e "\t${Y}53. ${G}Cror "
-echo
-echo -e "\t${Y}54. ${G}Name-that-hash"
-echo
-echo -e "\t${Y}55. ${G}Nikto "
-echo
-echo -e "\t${Y}56. ${G}Bmon "
-echo
-echo -e "\t${Y}57. ${G}zzuf "
-echo
-echo -e "\t${Y}58. ${G}Termux-am"
-echo
-echo -e "\t${Y}59. ${G}Termux-apt-repo "
-echo
-echo -e "\t${Y}60. ${G}Termux-create-package "
-echo
-echo -e "\t${Y}61. ${G}Openssh-sftp-server "
-echo
-echo -e "\t${Y}62. ${G}Openssl-tool "
-echo
-echo -e "\t${Y}63. ${G}Zig "
-echo
-echo -e "\t${Y}64. ${G}Util-linux-static "
-echo
-echo -e "\t${Y}65. ${G}Ruby-ri "
-echo
-echo -e "\t${Y}66. ${G}Termux-gui-bash "
-echo
-echo -e "\t${Y}67. ${G}Libxml2-python-static"
-echo
-echo -e "\t${Y}68. ${G}Root-repo"
-echo
-echo -e "\t${Y}69. ${G}Mautrix-whatsapp"
-echo
-echo -e "\t${Y}70. ${G}Recode-static"
-echo
-echo -e "\t${Y}71. ${G}Ytui-music"
-echo
-echo -e "\t${Y}72. ${G}Mtools "
-echo
-echo -e "\t${Y}73. ${G}Speedtest-go "
-echo
-echo -e "\t${Y}74. ${G}Telegram-cli "
-echo
-echo -e "\t${Y}75. ${G}Shell2http "
-echo
-echo -e "\t${Y}76. ${G}Python-cryptography "
-echo
-echo -e "\t${Y}77. ${G}Python-ensurepip-wheels "
-echo
-echo -e "\t${Y}78. ${G}Mesa-vulkan-icd-swrast "
-echo
-echo -e "\t${Y}79. ${G}Libde265-static "
-echo
-echo -e "\t${Y}80. ${G}Pcaudiolib-static "
-echo
-echo -e "\t${Y}81. ${G}Vulkan-loader-android"
-echo
-echo -e "\t${Y}82. ${G}Mpg123-stati "
-echo
-echo -e "\t${Y}83. ${G}Libcoinor-osi-static "
-echo
-echo -e "\t${Y}84. ${G}Krb5 "
-echo
-echo -e "\t${Y}85. ${G}Cupp"
-echo
-echo -e "\t${Y}86. ${G}Pkg-config "
-echo
-echo -e "\t${Y}87. ${G}Googletest "
-echo
-echo -e "\t${Y}88. ${G}Google-drive-upload"
-echo
-echo -e "\t${Y}89. ${G}Gnurl-static "
-echo
-echo -e "\t${Y}90. ${G}Go-findimagedupes"
-echo
-echo -e "\t${Y}91. ${G}Exit-Script "
-echo
-
+echo -e "\t${Y}12. ${G}SSh\n"
+echo -e "\t${Y}13. ${G}Wget\n"
+echo -e "\t${Y}14. ${G}curl\n"
+echo -e "\t${Y}15. ${G}Nmap\n"
+echo -e "\t${Y}16. ${G}tmux\n"
+echo -e "\t${Y}17. ${G}fish\n"
+echo -e "\t${Y}18. ${G}htop\n"
+echo -e "\t${Y}19. ${G}apache\n"
+echo -e "\t${Y}20. ${G}toilet\n"
+echo -e "\t${Y}21. ${G}vim\n"
+echo -e "\t${Y}22. ${G}Netcat\n"
+echo -e "\t${Y}23. ${G}sl\n"
+echo -e "\t${Y}24. ${G}Zip\n"
+echo -e "\t${Y}25. ${G}UnZip\n"
+echo -e "\t${Y}26. ${G}Java\n"
+echo -e "\t${Y}27. ${G}Exiftool\n"
+echo -e "\t${Y}28. ${G}Libcaca\n"
+echo -e "\t${Y}29. ${G}Termimage\n"
+echo -e "\t${Y}30. ${G}w3m\n"
+echo -e "\t${Y}31. ${G}fortune\n"
+echo -e "\t${Y}32. ${G}Cmatrix\n"
+echo -e "\t${Y}33. ${G}Screenfetch\n"
+echo -e "\t${Y}34. ${G}Tor\n"
+echo -e "\t${Y}35. ${G}tty-clock\n"
+echo -e "\t${Y}36. ${G}neofetch\n"
+echo -e "\t${Y}37. ${G}Moon-buggy\n"
+echo -e "\t${Y}38. ${G}Clang\n"
+echo -e "\t${Y}39. ${G}Proot-Distro\n"
+echo -e "\t${Y}40. ${G}Termux-api\n"
+echo -e "\t${Y}41. ${G}Cowsay\n"
+echo -e "\t${Y}42. ${G}Oneko\n"
+echo -e "\t${Y}43. ${G}Xeyes\n"
+echo -e "\t${Y}44. ${G}Hollywood\n"
+echo -e "\t${Y}45. ${G}Httrack\n"
+echo -e "\t${Y}46. ${G}mlocate\n"
+echo -e "\t${Y}47. ${G}Neovim\n"
+echo -e "\t${Y}48. ${G}Hashcat\n"
+echo -e "\t${Y}49. ${G}Hydra\n"
+echo -e "\t${Y}50. ${G}Metasploit-Framework\n"
+echo -e "\t${Y}51. ${G}Sqlmap\n"
+echo -e "\t${Y}52. ${G}John the Ripper\n"
+echo -e "\t${Y}53. ${G}Cror\n"
+echo -e "\t${Y}54. ${G}Name-that-hash\n"
+echo -e "\t${Y}55. ${G}Nikto\n"
+echo -e "\t${Y}56. ${G}Bmon\n"
+echo -e "\t${Y}57. ${G}zzuf\n"
+echo -e "\t${Y}58. ${G}Termux-am\n"
+echo -e "\t${Y}59. ${G}Termux-apt-repo\n"
+echo -e "\t${Y}60. ${G}Termux-create-package\n"
+echo -e "\t${Y}61. ${G}Openssh-sftp-server\n"
+echo -e "\t${Y}62. ${G}Openssl-tool\n"
+echo -e "\t${Y}63. ${G}Zig\n"
+echo -e "\t${Y}64. ${G}Util-linux-static\n"
+echo -e "\t${Y}65. ${G}Ruby-ri\n"
+echo -e "\t${Y}66. ${G}Termux-gui-bash\n"
+echo -e "\t${Y}67. ${G}Libxml2-python-static\n"
+echo -e "\t${Y}68. ${G}Root-repo\n"
+echo -e "\t${Y}69. ${G}Mautrix-whatsapp\n"
+echo -e "\t${Y}70. ${G}Recode-static\n"
+echo -e "\t${Y}71. ${G}Ytui-music\n"
+echo -e "\t${Y}72. ${G}Mtools\n"
+echo -e "\t${Y}73. ${G}Speedtest-go\n"
+echo -e "\t${Y}74. ${G}Telegram-cli\n"
+echo -e "\t${Y}75. ${G}Shell2http\n"
+echo -e "\t${Y}76. ${G}Python-cryptography\n"
+echo -e "\t${Y}77. ${G}Python-ensurepip-wheels\n"
+echo -e "\t${Y}78. ${G}Mesa-vulkan-icd-swrast\n"
+echo -e "\t${Y}79. ${G}Libde265-static\n"
+echo -e "\t${Y}80. ${G}Pcaudiolib-static\n"
+echo -e "\t${Y}81. ${G}Vulkan-loader-android\n"
+echo -e "\t${Y}82. ${G}Mpg123-stati\n"
+echo -e "\t${Y}83. ${G}Libcoinor-osi-static\n"
+echo -e "\t${Y}84. ${G}Krb5\n"
+echo -e "\t${Y}85. ${G}Cupp\n"
+echo -e "\t${Y}86. ${G}Pkg-config\n"
+echo -e "\t${Y}87. ${G}Googletest\n"
+echo -e "\t${Y}88. ${G}Google-drive-upload\n"
+echo -e "\t${Y}89. ${G}Gnurl-static\n"
+echo -e "\t${Y}90. ${G}Go-findimagedupes\n"
+echo -e "\t\a${Y}91. ${G}Exit-Script\n"
 for ((i=1; i<=4; i++))
 do
 echo
 done
 
 	echo -e "\033[91m"
-
 read -p "BOSS TOOL TO INSTALL : " choice
 
 for ((i=1; i<=3; i++))
 do
 echo
 done
-
 clear
-
-
 case $choice in 
     1) install_package python ;;
     2) install_package python2 ;;
